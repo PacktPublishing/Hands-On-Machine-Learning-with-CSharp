8e3b325dc81ff171426bd39ad7b492eb5d305607
suwarnarajput <packt.suwarnar@gmail.com>
2018-06-04 12:04:28 +0530

    Code files added

---
+++ Chapter11/src/EfficacySampler/EvolutionAlgorithmHost.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using SharpNeat.Core;
+using SharpNeat.Domains;
+using SharpNeat.EvolutionAlgorithms;
+using SharpNeat.Genomes.Neat;
+
+namespace EfficacySampler
+{
+    public class EvolutionAlgorithmHost
+    {
+        IGuiNeatExperiment _experiment;
+        StopCondition _stopCond;
+        NeatEvolutionAlgorithm<NeatGenome> _ea;
+        Stopwatch _stopwatch;
+
+        #region Constructor
+
+        public EvolutionAlgorithmHost(IGuiNeatExperiment experiment, StopCondition stopCond)
+        {
+            _experiment = experiment;
+            _stopCond = stopCond;
+            _stopwatch = new Stopwatch();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initialise and run the evolutionary algorithm until the stop condition occurs (either elapsed clock time, or some number of generations).
+        /// Once the stop conditon is reached this method returns with the current best fitness in the population.
+        /// </summary>
+        /// <returns></returns>
+        public Sample Sample()
+        {
+            _ea = CreateEvolutionAlgorithm();
+            _stopwatch.Restart();
+            _ea.StartContinue();
+
+            // Block the current thread until the EA stop condition occurs.
+            Block(_stopCond);
+
+            // Record stats as soon as we can (the EA is still running).
+            // Strictly speaking we should sync access to these states becase the EA is still running; and we prefer 
+            // not to stop it first because that can take a long time (because it waits for the current generation to complete).
+            Sample sample = new Sample();
+
+            lock(_ea.Statistics)
+            {
+                // Call Stop() here because we may have waited to obtain the lock, therefore this gives a more accurate
+                // measure of how much time had actually elapsed when the EA statistics were read.
+                _stopwatch.Stop();
+
+                sample.ElapsedTimeSecs = _stopwatch.ElapsedMilliseconds * 0.001;
+                sample.GenerationCount =  (int)_ea.CurrentGeneration;
+                sample.BestFitness = _ea.Statistics._maxFitness;
+                sample.MeanFitness = _ea.Statistics._meanFitness;
+                sample.MaxComplexity = _ea.Statistics._maxComplexity;
+                sample.MeanComplexity = _ea.Statistics._meanComplexity;
+                sample.EvaluationCount = _ea.Statistics._totalEvaluationCount;
+            }
+
+            // Request the EA to stop, and wait for it. 
+            // NB. This call could block for a long time, i.e. if the EA is within a long running generation then 
+            // it will not return until the generation has completed. Hence we prefer to read statistics above, and then
+            // call this method to actually stop the EA. 
+            _ea.RequestTerminateAndWait();
+
+            // Make some attempts at forcing release of resources (especially RAM) before we hand control back.
+            _ea = null;
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
+
+            return sample;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private NeatEvolutionAlgorithm<NeatGenome> CreateEvolutionAlgorithm()
+        {
+            int popSize = _experiment.DefaultPopulationSize;
+            IGenomeFactory<NeatGenome> genomeFactory = _experiment.CreateGenomeFactory();
+            List<NeatGenome> genomeList = genomeFactory.CreateGenomeList(popSize, 0);
+            NeatEvolutionAlgorithm<NeatGenome> ea = _experiment.CreateEvolutionAlgorithm(genomeFactory, genomeList);
+            return ea;
+        }
+
+        private void Block(StopCondition stopCond)
+        {
+            // Enter monitor loop.
+            if(stopCond.StopConditionType == StopConditionType.ElapsedClockTime) {
+                Block(TimeSpan.FromSeconds(stopCond.Value));
+            }
+            else {
+                Block(stopCond.Value);
+            }
+        }
+
+        private void Block(TimeSpan timespan)
+        {
+            const double thresholdMs = 10.0;
+
+            for(;;)
+            {
+                double remainingMs = (timespan - _stopwatch.Elapsed).TotalMilliseconds;
+                // No point calling Thread.Sleep for very short durations.
+                if(remainingMs <= thresholdMs || (RunState.Running != _ea.RunState)) {
+                    return;
+                }
+
+                // Wait for slightly less time than is remaining, to increase the chances of waiting the correct amount of time 
+                // (at time of writing Thread.Sleep does not appear to have especially accurate timing).
+                remainingMs = Math.Max(0, remainingMs - thresholdMs);
+                Thread.Sleep((int)remainingMs);
+            }
+        }
+
+        private void Block(int generation)
+        {
+            for(;;)
+            {
+                if((_ea.CurrentGeneration >= generation) || (RunState.Running != _ea.RunState)) {
+                    return;
+                }
+                Thread.Sleep(200);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Chapter11/src/EfficacySampler/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using log4net.Config;
+using SharpNeat.Domains;
+using SharpNeat.Domains.BinaryElevenMultiplexer;
+using SharpNeat.Domains.GenerativeFunctionRegression;
+
+namespace EfficacySampler
+{
+    class Program
+    {
+        static StreamWriter __streamWriter;
+
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += delegate {
+                if(null != __streamWriter) {
+                    __streamWriter.Close();
+                }
+            };
+
+            string experimentId;
+            string filename;
+            StopCondition stopCond = ArgUtils.ReadArgs(args, out experimentId, out filename);
+            if(null == stopCond) {
+                return;
+            }
+
+            // Initialise NEAT experiment.
+            IGuiNeatExperiment experiment = InitExperiment(experimentId);
+            if(null == experiment) {
+                return;
+            }
+
+            // Initialise log4net (log to console).
+            XmlConfigurator.Configure(new FileInfo("log4net.properties"));
+
+            // Initialise evolution algorithm host.
+            EvolutionAlgorithmHost eaHost = new EvolutionAlgorithmHost(experiment, stopCond);
+
+            __streamWriter = InitOutputFile(filename);
+            try
+            {
+                for(;;)
+                {
+                    Sample s = eaHost.Sample();
+                    __streamWriter.WriteLine($"{s.ElapsedTimeSecs},{s.GenerationCount},{s.BestFitness:0.#####},{s.MeanFitness:0.#####},{s.MaxComplexity:0.#####},{s.MeanComplexity:0.#####},{s.EvaluationCount}");
+                    __streamWriter.Flush();
+                }
+            }
+            finally
+            {
+                if(null != __streamWriter) {
+                    __streamWriter.Close();
+                }
+            }
+        }
+
+        #region Private Static Methods
+
+        private static StreamWriter InitOutputFile(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+            if(fileInfo.Exists)
+            {
+                // Append to existing file.
+                return new StreamWriter(filename, true);
+            }
+            
+            // Create new file and write a header row.
+            StreamWriter sw = new StreamWriter(filename);
+            sw.WriteLine("secs,gens,bestfitness,meanfitness,maxcomplexity,meancomplexity,evalcount");
+            return sw;
+        }
+
+        private static IGuiNeatExperiment InitExperiment(string experimentId)
+        {
+            switch(experimentId)
+            {
+                case "binary11":
+                    return InitExperiment_BinaryElevenMultiplexer();
+                case "sinewave":
+                    return InitExperiment_Sinwave();
+            }
+
+            Console.WriteLine($"Unrecognised experiment [{experimentId}]");
+            return null;
+        }
+
+        private static IGuiNeatExperiment InitExperiment_BinaryElevenMultiplexer()
+        {
+            // Experiment classes encapsulate much of the nuts and bolts of setting up a NEAT search.
+            var experiment = new BinaryElevenMultiplexerExperiment();
+
+            // Load config XML.
+            XmlDocument xmlConfig = new XmlDocument();
+            xmlConfig.Load("config/binary-eleven-multiplexer.config.xml");
+            experiment.Initialize(experiment.Name, xmlConfig.DocumentElement);
+            return experiment;
+        }
+
+        private static IGuiNeatExperiment InitExperiment_Sinwave()
+        {
+            // Experiment classes encapsulate much of the nuts and bolts of setting up a NEAT search.
+            var experiment = new GenerativeFnRegressionExperiment();
+
+            // Load config XML.
+            XmlDocument xmlConfig = new XmlDocument();
+            xmlConfig.Load("config/generative-sinewave.config.xml");
+            experiment.Initialize(experiment.Name, xmlConfig.DocumentElement);
+            return experiment;
+        }
+
+        #endregion
+    }
+}
